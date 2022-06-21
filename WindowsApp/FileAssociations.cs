f6bfb903bd8074a2ea592ca9cff8d7be709ff9d2
Sittipon Simasanti <sittipon@api.co.th>
2022-06-21 14:44:44 +0700

    Added file associations

---
+++ WindowsApp/FileAssociations.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperPlane2
+{
+
+    public class FileAssociations
+    {
+        [System.Runtime.InteropServices.DllImport("Shell32.dll")]
+        private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
+
+        private const int SHCNE_ASSOCCHANGED = 0x8000000;
+        private const int SHCNF_FLUSH = 0x1000;
+
+        public static bool SetAssociation(string extension, string progId, string fileTypeDescription, string applicationFilePath)
+        {
+            bool madeChanges = false;
+            madeChanges |= SetKeyDefaultValue(@"Software\Classes\" + extension, progId);
+            madeChanges |= SetKeyDefaultValue(@"Software\Classes\" + progId, fileTypeDescription);
+            madeChanges |= SetKeyDefaultValue($@"Software\Classes\{progId}\shell\open\command", "\"" + applicationFilePath + "\" \"%1\"");
+            return madeChanges;
+        }
+
+        private static bool SetKeyDefaultValue(string keyPath, string value)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(keyPath))
+            {
+                if (key.GetValue(null) as string != value)
+                {
+                    key.SetValue(null, value);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- WindowsApp/Program.cs
+++ WindowsApp/Program.cs
@@ -6,18 +6,25 @@ namespace PaperPlane2
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(string [] arguments)
+        static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
+            string[] args = Environment.GetCommandLineArgs();
+            FileAssociations.SetAssociation(".pdf", "PaperPlane.PDF", "Use Paper Plane to split, merge, rearrange PDF files", args[0]);
+
             string? fileName = null;
-            foreach (var arg in arguments) {
-                if (arg.ToLower().EndsWith(".pdf")) {
-                    fileName = arg;
+            foreach (var arg in args) {
+                if (!arg.ToLower().EndsWith(".pdf")) {
+                    continue;
                 }
+                fileName = arg;
+                break;
             }
+
+
             Application.Run(new MainForm(fileName));
         }
     }
